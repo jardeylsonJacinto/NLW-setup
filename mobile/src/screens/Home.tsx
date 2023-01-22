fd9ca29c3d87c048105e9684fd534fa856f374c0
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-01-22 09:11:08 -0300

    feat: mobile - create generate progress percentage

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -3,7 +3,7 @@ import { Text, View, ScrollView, Alert } from 'react-native';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 import { api } from '../lib/axios';
-import { generateRangeDatesFromYearStart } from '../utils/generate-range-dates-from-year-start';
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-beteween-dates';
 
 import { Header } from '../components/Header';
 import { Loading } from '../components/Loading';
